b4ad4b294590d19f8adb0acba3b74f3bc7f29b80
Francis <francis@aspgems.com>
2013-04-05 10:32:38 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EagerLoadPolymorphicErrorBugDemo::Application.config.secret_token = '2c50778fa8384aba10b4ae413ffdf7057f530d87cc3d4e558882625f510b156f2940c617903a645e6a92dde70c0c76b3529c712c6339c773b11bceac6dbbaeee'
